037be0e997ca52947775a36c60d48ea63632c52b
JonGMS <joaoggabriell@gmail.com>
2026-03-08 22:55:37 -0300

    Validadores de opções

--- Program.cs
+++ Program.cs
@@ -15,161 +15,122 @@ class HelloWorld
     string[] senha = { "ADMIN", "123", "321", "AMOR DA MINHA VIDA" };
 
     Login(usuario, senha);
-
+    int contador = 0;
     while (true)
     {
       int opcao = ApresentarMenuPrincipal();
+     
 
       if (opcao == 1)//Calculadora
       {
-        Console.Clear();
-        string operador = ApresentarMenu();
-
-        int condicao = 0;
-        int contador = 0;
-
-        while (condicao < 1)
+        string operador;
+        
+        do
         {
-          ApresentarTitulo();
-
-          Console.Write("Primeiro Número:");
-          string strPrimeiroNumero = Console.ReadLine();
-          if (strPrimeiroNumero == "SAIR")
-          {
-            condicao++;
-            continue;
-          }
-          decimal primeiroNumero = Convert.ToDecimal(strPrimeiroNumero);
-
-          Console.Write(operador + "\n");
-
-          Console.Write("Segundo Número: ");
-          string strSegundoNumero = Console.ReadLine();
-
-          if (operador == "/" && strSegundoNumero == "0")
-          {
-            for (int i = 0; i == 0;)
-            {
-              ApresentarComDestaque("Não é possivel divisão por 0 (ZERO)", ConsoleColor.Red);
-              Console.Write("\nSegundo Número: ");
-              strSegundoNumero = Console.ReadLine();
-              if (strSegundoNumero != "0" && strSegundoNumero != "")
-              {
-                i = 1;
-              }
-            }
-          }
-          if (strSegundoNumero == "SAIR")
-          {
-            condicao++;
-            continue;
-
-          }
-          decimal segundoNumero = Convert.ToDecimal(strSegundoNumero);
-
-
-          Calculadora calculadora = new Calculadora(primeiroNumero, segundoNumero);
-
-          decimal resultado = calculadora.Calcular(operador, primeiroNumero, segundoNumero);
-
-          historicoCalculadora[contador] = primeiroNumero + " " + operador + " " + segundoNumero + " = " + resultado;
-
-          Console.ForegroundColor = ConsoleColor.DarkBlue;
-          Console.WriteLine(primeiroNumero + " " + operador + " " + segundoNumero + " = " + resultado + "\n");
-          Console.ResetColor();
-          Console.Write("Para continuar aparte [ENTER]");
-          Console.ReadLine();
           Console.Clear();
-          contador++;
-        }
-
-        for (int i = 0; i < historicoCalculadora.Length; i++)
-        {
-          string item = historicoCalculadora[i];
-
-          if (item.Contains("+") || item.Contains("-") || item.Contains("/") || item.Contains("*"))
+          operador = ObterOperador();
+          if (operador == "SAIR")
           {
-            Console.WriteLine(historicoCalculadora[i]);
+            continue;
           }
 
-        }
+          Calculadora(operador, contador, historicoCalculadora);
+
+          for (int i = 0; i < historicoCalculadora.Length; i++)
+          {
+            string item = historicoCalculadora[i];
+            if (!string.IsNullOrEmpty(item))
+            {
+              Console.WriteLine(historicoCalculadora[i]);
+            }
+
+          }
+          ApresentarComDestaque("Aperte [ENTER] para continuar!", ConsoleColor.DarkCyan);
+          Console.ReadLine();
+        } while (operador != "SAIR");
+
+
       }
       else if (opcao == 2)//Simulador de cadastro
       {
-        bool argumento = true;
-
-        while (argumento)
+        string opcaoAluno = "";
+        do
         {
-          Console.Write("Digite "); ApresentarComDestaque("[1] para CADASTRAR ALUNO\n", ConsoleColor.Blue);
-          Console.Write("Digite "); ApresentarComDestaque("[1] para CADASTRAR ALUNO\n", ConsoleColor.Cyan);
-          string opcaoAluno = Console.ReadLine();
+           opcaoAluno = ApresentarMenu("FORMULÁRIO");
+          string nome = "";
+
           if (opcaoAluno == "1")
           {
-            Console.Write("Digite o nome ou [SAIR] para sair: ");
-            string nome = Console.ReadLine();
-            if (nome == "SAIR")
+            do
             {
-              argumento = false;
-              continue;
-            }
+              ApresentarTitulo("Simulador de Formulário", ConsoleColor.DarkCyan);
 
-            Console.Write("Digite a idade: ");
-            int idade = Convert.ToInt16(Console.ReadLine());
-
-            if (idade < 18)
-            {
-              ApresentarComDestaque("Você é menor de Idade\n", ConsoleColor.Cyan);
-            }
-            else
-            {
-              ApresentarComDestaque("Você é maior de Idade\n", ConsoleColor.DarkBlue);
-            }
-
-            Console.Write("Digite a a altura: ");
-            decimal altura = Convert.ToDecimal(Console.ReadLine());
-
-            Console.Write("Digite o peso: ");
-            decimal peso = Convert.ToDecimal(Console.ReadLine());
-
-            Console.Write("Digite o endereço: ");
-            string endereco = Console.ReadLine();
-
-            decimal[] provas = new decimal[4];
-
-            for (int i = 0; i < provas.Length; i++)
-            {
-              int j = 0;
-              while (j == 0)
+              Console.Write("Digite o nome ou [SAIR] para sair: ");
+              nome = Console.ReadLine();
+              if (nome == "SAIR")
               {
-                try
-                {
-                  Console.Write($"Digite a nota {i + 1}: ");
-                  string strNota = Console.ReadLine();
-                  decimal nota = Convert.ToDecimal(strNota);
-                  if (nota > 10 || nota < 0)
-                  {
-                    ApresentarComDestaque("Nota inválida, tente novamente.\n", ConsoleColor.Red);
-                    continue;
-                  }
-                  j++;
-                  provas[i] = nota;
-                }
-                catch (Exception e)
-                {
-                  ApresentarComDestaque("Nota inválida, tente novamente.", ConsoleColor.Red);
-                }
+
+                continue;
               }
 
-            }
+              Console.Write("Digite a idade: ");
+              int idade = Convert.ToInt16(Console.ReadLine());
 
-            Aluno aluno = new Aluno(nome, idade, altura, peso, endereco, provas[0], provas[1], provas[2], provas[3]);
-            listaAluno.Add(aluno);
+              if (idade < 18)
+              {
+                ApresentarComDestaque("Você é menor de Idade\n", ConsoleColor.Cyan);
+              }
+              else
+              {
+                ApresentarComDestaque("Você é maior de Idade\n", ConsoleColor.DarkBlue);
+              }
 
-            Console.Clear();
+              Console.Write("Digite a a altura: ");
+              decimal altura = Convert.ToDecimal(Console.ReadLine());
 
+              Console.Write("Digite o peso: ");
+              decimal peso = Convert.ToDecimal(Console.ReadLine());
 
-            ApresentarMensagem(nome, idade, altura, peso, endereco);
+              Console.Write("Digite o endereço: ");
+              string endereco = Console.ReadLine();
+
+              decimal[] provas = new decimal[4];
+
+              for (int i = 0; i < provas.Length; i++)
+              {
+                int j = 0;
+                while (j == 0)
+                {
+                  try
+                  {
+                    Console.Write($"Digite a nota {i + 1}: ");
+                    string strNota = Console.ReadLine();
+                    decimal nota = Convert.ToDecimal(strNota);
+                    if (nota > 10 || nota < 0)
+                    {
+                      ApresentarComDestaque("Nota inválida, tente novamente.\n", ConsoleColor.Red);
+                      continue;
+                    }
+                    j++;
+                    provas[i] = nota;
+                  }
+                  catch (Exception e)
+                  {
+                    ApresentarComDestaque("Nota inválida, tente novamente.", ConsoleColor.Red);
+                  }
+                }
+
+              }
+
+              Aluno aluno = new Aluno(nome, idade, altura, peso, endereco, provas[0], provas[1], provas[2], provas[3]);
+              listaAluno.Add(aluno);
+
+              Console.Clear();
+
+              ApresentarMensagem(nome, idade, altura, peso, endereco);
+            } while (nome != "SAIR");
           }
+
           else if (opcaoAluno == "2")
           {
             foreach (Aluno dadoAluno in listaAluno)
@@ -183,20 +144,17 @@ class HelloWorld
             ApresentarComDestaque("Opcao inválida! Tente novamente apertando [ENTER]", ConsoleColor.Red);
             Console.ReadLine();
           }
-
-        }
+        } while (opcaoAluno != "0");
 
       }
       else if (opcao == 3)
       {
-        Console.Clear();
-        Console.Write("Digite "); ApresentarComDestaque("[1] para adicionar notas (Randomicas)\n", ConsoleColor.DarkBlue);
-        Console.Write("Digite "); ApresentarComDestaque("[2] para Apresentar Boletim\n", ConsoleColor.Cyan);
-        string strOpcao = Console.ReadLine();
+        string strOpcao = ApresentarMenu("BOLETIM");
+
         if (strOpcao == "1")
         {
           string[] nomeAluno = { "Rogério Silva", "Lucas Andrade", "Fernando Costa", "Gabriel Souza", "Ricardo Oliveira", "Tiago Martins", "Bruno Carvalho", "Larissa Cordova", "Ana Paula Mendes", "Juliana Ferreira", "Camila Rocha", "Mariana Santos", "Patrícia Almeida", "Bianca Teixeira", "João Gabriel Santos" };
-          int[] idadeAluno = { 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13};
+          int[] idadeAluno = { 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13 };
 
           int[] idAluno = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
           Random nmrRandon = new Random();
@@ -215,7 +173,7 @@ class HelloWorld
             int[] provas = new int[4];
             for (int j = 0; j < provas.Length; j++)
             {
-              int nota = notasRandon.Next(4, 10);
+              int nota = notasRandon.Next(5, 10);
               provas[j] = nota;
             }
 
@@ -227,27 +185,28 @@ class HelloWorld
         }
         else if (strOpcao == "2")
         {
-          if(listaAluno.Count > 0)
+          if (listaAluno.Count > 0)
           {
             Console.Clear();
             ApresentarComDestaque("   ALUNO   | NOTA 1 | NOTA 2 | NOTA 3 | NOTA 4 | MÉDIA | STATUS\n", ConsoleColor.DarkGray);
             foreach (Aluno dadoAluno in listaAluno)
             {
-              string nomeLimitado = dadoAluno.Nome.Substring(0,11);
+              string nomeLimitado = dadoAluno.Nome.Substring(0, 11);
               decimal soma = dadoAluno.Prova1 + dadoAluno.Prova2 + dadoAluno.Prova3 + dadoAluno.Prova4;
               decimal media = soma / 4;
 
-              Console.Write(nomeLimitado+ "|   "+ dadoAluno.Prova1+ "    "+"|   "+ dadoAluno.Prova2+ "    "+"|   "+ dadoAluno.Prova3+ "    "+"|   "+ dadoAluno.Prova4+ "    " + "|   ");
-              if(media > 7)
+              Console.Write(nomeLimitado + "|   " + dadoAluno.Prova1 + "    " + "|   " + dadoAluno.Prova2 + "    " + "|   " + dadoAluno.Prova3 + "    " + "|   " + dadoAluno.Prova4 + "    " + "|  ");
+              if (media > 7)
               {
-                ApresentarComDestaque(media.ToString("F1")  + "     |APROVADO\n", ConsoleColor.Green);
+                ApresentarComDestaque(media.ToString("F1") + "  |APROVADO\n", ConsoleColor.Green);
               }
               else
               {
-                ApresentarComDestaque(media.ToString("F1")  +  "     |REPROVADO\n", ConsoleColor.Red);
+                ApresentarComDestaque(media.ToString("F1") + "  |REPROVADO\n", ConsoleColor.Red);
               }
-              
+
             }
+            ApresentarComDestaque("Boletim apresentado com sucesso! aperte [ENTER] para continuar.", ConsoleColor.Green);
             Console.ReadLine();
           }
           else
@@ -255,8 +214,12 @@ class HelloWorld
             ApresentarComDestaque("Nenhum aluno ou nota cadastrado!! Aperte [ENTER] para tentar novamente.", ConsoleColor.Red);
             Console.ReadLine();
           }
-
+          
         }
+      }
+      else if (opcao == 0)
+      {
+        break;
       }
     }
 
@@ -329,22 +292,51 @@ class HelloWorld
     Console.ResetColor();
 
   }
-  public static void ApresentarTitulo()//Calculadora
+  public static void ApresentarTitulo(string text, ConsoleColor cor)//Calculadora
   {
     Console.Clear();
-    Console.WriteLine("Calculadora 1.0");
-    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(text);
+    Console.ForegroundColor = cor;
     Console.WriteLine("================");
     Console.ResetColor();
   }
-  public static string ApresentarMenu()//Calculadora
+  public static string ApresentarMenu(string tipo)
   {
+    Console.Clear();
+    if (tipo == "CALCULADORA")
+    {
+      Console.WriteLine("Digite [1] para SOMA\nDigite [2] para SUBTRAÇÃO\nDigite [3] para DIVISÃO\nDigite [4] para MULTIPLICAÇÃO\nDigite [0] para SAIR\n");
+      Console.Write("Operação: "); string opcao = Console.ReadLine();
+      return opcao;
+    }
+    else if (tipo == "FORMULÁRIO")
+    {
+      Console.Clear();
+      Console.Write("Digite "); ApresentarComDestaque("[1] para CADASTRAR ALUNO\n", ConsoleColor.Blue);
+      Console.Write("Digite "); ApresentarComDestaque("[2] para LISTAR ALUNOS\n", ConsoleColor.Cyan);
+      Console.Write("Digite "); ApresentarComDestaque("[0] para VOLTAR\n", ConsoleColor.Red);
+      string opcao = Console.ReadLine();
+      return opcao;
+    }
+    else if (tipo == "BOLETIM")
+    {
+      Console.Clear();
+      Console.Write("Digite "); ApresentarComDestaque("[1] para adicionar notas (Randomicas)\n", ConsoleColor.DarkBlue);
+      Console.Write("Digite "); ApresentarComDestaque("[2] para Apresentar Boletim\n", ConsoleColor.Cyan);
+      Console.Write("Digite "); ApresentarComDestaque("[0] para SAIR\n", ConsoleColor.Red);
+      string opcao = Console.ReadLine();
+      return opcao;
+    }
 
+    return "";
+  }
+  public static string ObterOperador()
+  {
     while (true)
     {
-      ApresentarTitulo();
-      Console.WriteLine("Digite [1] para SOMA\nDigite [2] para SUBTRAÇÃO\nDigite [3] para DIVISÃO\nDigite [4] para MULTIPLICAÇÃO\n");
-      string opcao = Console.ReadLine();
+      ApresentarTitulo("Calculadora 1.0", ConsoleColor.DarkBlue);
+
+      string opcao = ApresentarMenu("CALCULADORA");
 
       if (opcao == "1")
       {
@@ -362,6 +354,10 @@ class HelloWorld
       {
         return "*";
       }
+      else if (opcao == "0")
+      {
+        return "SAIR";
+      }
       else
       {
         Console.ForegroundColor = ConsoleColor.Red;
@@ -373,9 +369,69 @@ class HelloWorld
       }
 
     }
+  }
+  public static void Calculadora(string operador, int contador, string[] historicoCalculadora)
+  {
+    int condicao = 0;
+    do
+    {
+      ApresentarTitulo("Calculadora 1.0", ConsoleColor.DarkBlue);
+
+      Console.Write("Primeiro Número:");
+      string strPrimeiroNumero = Console.ReadLine();
+
+      if (strPrimeiroNumero == "SAIR")
+      {
+        return;
+      }
+      decimal primeiroNumero = Convert.ToDecimal(strPrimeiroNumero);
+
+      Console.Write(operador);
+
+      Console.Write("\nSegundo Número: ");
+      string strSegundoNumero = Console.ReadLine();
+
+      if (operador == "/" && strSegundoNumero == "0")
+      {
+        for (int i = 0; i == 0;)
+        {
+          ApresentarComDestaque("Não é possivel divisão por 0 (ZERO)", ConsoleColor.Red);
+          Console.Write("\nSegundo Número: ");
+          strSegundoNumero = Console.ReadLine();
+          if (strSegundoNumero != "0" && strSegundoNumero != "")
+          {
+            i = 1;
+          }
+        }
+      }
+      if (strSegundoNumero == "SAIR")
+      {
+        return;
+      }
+      decimal segundoNumero = Convert.ToDecimal(strSegundoNumero);
+
+
+      Calculadora calculadora = new Calculadora(primeiroNumero, segundoNumero);
+
+      decimal resultado = calculadora.Calcular(operador, primeiroNumero, segundoNumero);
+
+      historicoCalculadora[contador] = primeiroNumero + " " + operador + " " + segundoNumero + " = " + resultado;
+
+      Console.ForegroundColor = ConsoleColor.DarkBlue;
+      Console.WriteLine(primeiroNumero + " " + operador + " " + segundoNumero + " = " + resultado + "\n");
+      Console.ResetColor();
+      Console.Write("Para continuar aparte [ENTER] para sair digite [SAIR]: ");
+      string resposta = Console.ReadLine();
+      if (resposta == "SAIR" || resposta == "Sair" || resposta == "sair")
+      {
+        return;
+      }
+      Console.Clear();
+      contador++;
+    } while (condicao < 1);
+
 
   }
-
 
 
 }
@@ -414,7 +470,7 @@ class Aluno
   }
 
   float[] lista_notas = new float[4];
-  
+
 
 }
 class Calculadora
